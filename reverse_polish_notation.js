1df50cb9fa59b22df9faa4fbf0fa68ef73e4e5e7
romanwit <romanforresume@gmail.com>
2016-10-15 19:10:11 +0300

    example of processing of reverse polish notation

---
+++ reverse_polish_notation.js
@@ -0,0 +1,41 @@
+var x = ["4", "13", "5", "/", "+"];
+var y = evalRPN(x);
+alert(y);
+
+function evalRPN(tokens) {
+    var t;
+    var num;
+	var i = 0;
+	while (i<tokens.length) {
+		
+        t = tokens[i];
+        num = 0;
+		switch (t) {
+			case "+": 
+				num = parseInt(tokens[i-2]) + parseInt(tokens[i-1]);
+				break;			
+			case "-":
+				num = parseInt(tokens[i-2]) - parseInt(tokens[i-1]);
+				break;
+			
+			case "*":
+				num = parseInt(tokens[i-2]) * parseInt(tokens[i-1]);
+				break;
+			
+			case "/":
+				num = Math.trunc(parseInt(tokens[i-2]) / parseInt(tokens[i-1]));
+				break;
+		}
+		
+		if ((t=="*")||(t=="/")||(t=="-")||(t=="+")) {
+			tokens.splice(i + 1, 0, num);
+			tokens.splice(i-2, 3);
+			i = 0;
+		}
+		else {
+			i++;
+		}
+        
+    }
+	return(tokens[0]);
+};
